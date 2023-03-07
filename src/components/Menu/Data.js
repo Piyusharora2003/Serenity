0cf140c7bca87c3fa6d6b54a94889ccce22a6d55
gautam <buddhgautam777@gmail.com>
2023-03-07 21:46:46 +0530

    moves, music bn gaye

--- src/components/Menu/Data.js
+++ src/components/Menu/Data.js
@@ -3,7 +3,7 @@ export const Data=[
    id:1,
    image:"https://cdn-in.icons8.com/yPqGjFz4y06lAX9IolKbzw/-4kkJFMeG0CkFSNteO1Ndg/Musical_Notes.png",
    link:"/music",
-   info:"Music is an art form that uses sound and rhythm to creof styles, genres, and cultures, and has been an integral part of human history and culture for thousands of years.",
+   info:"Music has the power to touch your soul. Different languages create different moods, different moods demand different songs. Explore the different styles, genres and cultures of rhythmic sounds that describes you best, and heals your soul.",
    title:"Music",
    bgcolor:"#D2D4D6"
    },
@@ -11,7 +11,7 @@ export const Data=[
       id:2,
       image:"https://cdn-in.icons8.com/yPqGjFz4y06lAX9IolKbzw/KiNfmOi8v028UM_ZwC1_yg/Cinema.png",
       link:"/movies",
-      info:"Music is an art form that uses sound and rhythm to creof styles, genres, and cultures, and has been an integral part of human history and culture for thousands of years.",
+      info:"Movies introduce you to not only one, but a varied art form. It can motivate, encourage, and romanticise you, based on what character you admire the most. However hectic, boring, stressful your present is, your near future could always be exciting in your own way. Explore what role is your life demanding now.",
       title:"Movies",
       bgcolor:"#D1E8E4"
 
